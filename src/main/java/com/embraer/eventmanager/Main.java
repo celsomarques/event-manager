8c6c07f5770a5b59fc6f58c6ecaef1e53919f4c4
Celso Marques <celso.marques82@gmail.com>
2015-01-14 17:09:44 -0200

    Add jetty dependencies and Procfile

---
+++ src/main/java/com/embraer/eventmanager/Main.java
@@ -0,0 +1,33 @@
+package com.embraer.eventmanager;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * @author Celso Ricardo Marques Pereira - CRMPERE
+ * @version 1.0 - 14/01/2015
+ */
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		String webappDirLocation = "src/main/webapp";
+		
+		String webPort = System.getenv("PORT");
+		if (StringUtils.isBlank(webPort)) {
+			webPort = "8080";
+		}
+		
+		Server server = new Server(Integer.valueOf(webPort));
+		WebAppContext root = new WebAppContext();
+		root.setContextPath("/");
+		root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+		root.setResourceBase(webappDirLocation);
+		root.setParentLoaderPriority(true);
+		
+		server.setHandler(root);
+		server.start();
+		server.join();
+	}
+}
