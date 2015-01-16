957126b539976bf550e7cf6e9768592b885e78e7
Celso Marques <celso.marques82@gmail.com>
2015-01-16 09:07:46 -0200

    .

--- src/main/java/com/embraer/eventmanager/EventController.java
+++ src/main/java/com/embraer/eventmanager/EventController.java
@@ -2,6 +2,7 @@ package com.embraer.eventmanager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,5 +21,13 @@ public class EventController {
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
 	public @ResponseBody Iterable<Event> findAll() {
 		return this.repository.findAll();
+	}
+	
+	@RequestMapping(value = "{businessDocumentNumber}", method = RequestMethod.GET)
+	public @ResponseBody Event create(@PathVariable("businessDocumentNumber") String businessDocumentNumber) {
+		
+		Event event = new Event();
+		event.setBusinessDocumentNumber(businessDocumentNumber);
+		return this.repository.save(event);
 	}
 }
